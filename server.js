4424b3e656115b1984559c89f1b8c35dc207edf7
Karin Keijzer <karin.keijzer@capgemini.com>
2019-04-08 19:25:36 +0200

    Danger noodle - Empty project structure

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Danger Noodle!'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/static/index.html')));
 
-app.listen(port, () => console.log(`Danger Noodle listening on port ${port}!`));
+app.use('/static', express.static(__dirname + '/static'));
+
+const dangerNoodleIntro = "" +
+    "                                                ___ \n" +
+    `    ^   Danger Noodle listening on port ${port}!  / *_>-<\n` +
+    "   | |            _____________               / / \n" +
+    "   | \\___________/ ___________ \\_____________/ / \n" +
+    "    \\_____________/           \\_______________/ \n";
+app.listen(port, () => console.log(dangerNoodleIntro));
